bb4bcbe44705ee1f71795e80490bdcb61fc629d6
Anton Petrov <petrov.aap@gmail.com>
2018-05-16 00:10:12 +0300

    Added ability to supscribe on IAS Zone reports, added support for some Megafon (Nexturn) devices

--- scripts/permit-join.js
+++ scripts/permit-join.js
@@ -1,57 +1,62 @@
 const zigbee = require('../lib/zigbee')
+const findSerialPort = require('../lib/utils/findSerialPort')
 
-const PORT = process.argv[2]
+async function init() {
+  zigbee.init({
+    port: process.argv[2] || await findSerialPort(),
+    db: './shepherd.db',
+  })
 
-if (!PORT) {
-  throw new Error('Serial port is not specified')
+  zigbee.on('error', (error) => {
+    console.error('Error:', error)
+  })
+
+  zigbee.on('ready', () => {
+    console.log('Ready')
+    setTimeout(() => {
+      zigbee.permitJoin(120, (error) => {
+        if (error) {
+          console.error('Error:', error)
+        }
+      })
+    }, 3000)
+  })
+
+  zigbee.on('permitJoining', (joinTimeLeft) => {
+    console.log('Permit joining:', joinTimeLeft)
+  })
+
+  zigbee.on('ind', (msg) => {
+    switch (msg.type) {
+      case 'devInterview':
+        console.log(`Interview endpoint: ${msg.status.endpoint.current} of ${msg.status.endpoint.total} cluster ${msg.status.endpoint.cluster.current} of ${msg.status.endpoint.cluster.total}`)
+        break
+      case 'devIncoming':
+        console.log(`Device joined, id: ${msg.data}`)
+        break
+      case 'devLeaving':
+        console.log(`Device announced leaving and is removed, id: ${msg.data}`)
+        break
+      case 'attReport':
+        console.log('Report:', msg)
+        break
+      case 'statusChange':
+      console.log('Status:', msg.endpoints[0].device.ieeeAddr)
+      console.log('Status:', msg.data)
+        break
+      case 'devStatus':
+        console.log('Dev status:', msg.data)
+        break
+      default:
+        console.log('Unrecognized message:', msg)
+    }
+  })
+
+  zigbee.start((error) => {
+    if (error) {
+      console.error('Error:', error)
+    }
+  })
 }
 
-zigbee.init({
-  port: PORT,
-  db: './shepherd.db',
-})
-
-zigbee.on('error', (error) => {
-  console.error('Error:', error)
-})
-
-zigbee.on('ready', () => {
-  console.log('Ready')
-  setTimeout(() => {
-    zigbee.permitJoin(120, (error) => {
-      if (error) {
-        console.error('Error:', error)
-      }
-    })
-  }, 3000)
-})
-
-zigbee.on('permitJoining', (joinTimeLeft) => {
-  console.log('Permit joining:', joinTimeLeft)
-})
-
-zigbee.on('ind', (msg) => {
-  switch (msg.type) {
-    case 'devInterview':
-      console.log(`Interview endpoint: ${msg.status.endpoint.current} of ${msg.status.endpoint.total} cluster ${msg.status.endpoint.cluster.current} of ${msg.status.endpoint.cluster.total}`)
-      break
-    case 'devIncoming':
-      console.log(`Device joined, id: ${msg.data}`)
-      break
-    case 'devLeaving':
-      console.log(`Device announced leaving and is removed, id: ${msg.data}`)
-      break
-    case 'attReport':
-      console.log('Report:', msg)
-      break
-    case 'statusChange':
-      console.log('Status:', msg)
-      break
-  }
-})
-
-zigbee.start((error) => {
-  if (error) {
-    console.error('Error:', error)
-  }
-})
+init()
